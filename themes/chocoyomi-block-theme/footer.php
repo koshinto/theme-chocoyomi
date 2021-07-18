ffa9e189cd76dc3e34a698a5d81ddda94d0fed81
Kosuke Shinto <kou.shinto@icloud.com>
2021-07-19 07:16:56 +0900

    change フッター・ヘッダーメニュー

--- themes/chocoyomi-block-theme/footer.php
+++ themes/chocoyomi-block-theme/footer.php
@@ -1,8 +1,7 @@
 <footer id="footer">
-  <div class="container">
-    <section>
-      <h1><?php bloginfo('name'); ?></h1>
-      <p><?php bloginfo('description'); ?></p>
+    <section id="footer-blog-name">
+      <h1 id="footer-blog-title"><?php bloginfo('name'); ?></h1>
+      <p id="footer-blog-description"><?php bloginfo('description'); ?></p>
     </section>
     <?php wp_nav_menu(array(
         'container' => 'nav',
@@ -10,15 +9,14 @@
         'theme_location' => 'footer-menu'
       ));
     ?>
-    <section>
-      <ul id="share-button">
+    <section id="footer-share-button">
+      <ul class="share-button">
         <li><a href="https://twitter.com/intent/tweet?text=<?php echo home_url(); ?> <?php echo get_site_url(); ?> <twitter account>"><i class="fab fa-twitter"></i></a></li>
         <li><a href="http://www.facebook.com/sharer.php?u=<?php echo home_url(); ?>&amp;t=<?php bloginfo('name'); ?>"><i class="fab fa-facebook"></i></a></li>
         <li><a href="https://social-plugins.line.me/lineit/share?url=<?php echo home_url(); ?>"><i class="fab fa-line"></i></a></li>
       </ul>
     </section>
-    <small>&copy; <?php bloginfo('name'); ?></small>
-  </div>
+    <small id="copyright">&copy; <?php bloginfo('name'); ?></small>
 </footer>
 <?php wp_footer(); ?>
 </body>
