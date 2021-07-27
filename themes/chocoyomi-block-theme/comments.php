6cc2904de0d5db67f5b49a39fb100a527082b111
Kosuke Shinto <kou.shinto@icloud.com>
2021-07-27 14:15:39 +0900

    change コメントリストのスタイル

--- themes/chocoyomi-block-theme/comments.php
+++ themes/chocoyomi-block-theme/comments.php
@@ -1,11 +1,11 @@
 <section id="comment-form">
   <?php if(have_comments()): ?>
-    <div class="caption">
+    <div class="sec-title">
       <p>Comments</p>
       <h2>この記事のコメント</h2>
     </div>
     <ul id="comment-list">
-      <?php wp_list_comments('max_depth=1&format=html5'); ?>
+      <?php wp_list_comments('max_depth=1&format=html5&avatar_size=48'); ?>
     </ul>
   <?php endif; ?>
   <?php comment_form('format=html5'); ?>
