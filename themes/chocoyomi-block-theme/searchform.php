a774314e06cb08943d3176cf4d307cb101f51b7f
Kosuke Shinto <kou.shinto@icloud.com>
2021-08-01 23:53:50 +0900

    fix 検索フォーム修正

--- themes/chocoyomi-block-theme/searchform.php
+++ themes/chocoyomi-block-theme/searchform.php
@@ -1,4 +1,4 @@
-<form method="get" id="header-search-form" action="<?php echo esc_url(home_url('/')); ?>">
-  <input type="search" placeholder="キーワード検索" name="s" value="" />
+<form method="get" id="search-form" action="<?php echo esc_url(home_url('/')); ?>">
   <button type="submit"><i class="fas fa-search"></i></button>
+  <input type="search" placeholder="キーワード検索" name="s" value="" />
 </form>
