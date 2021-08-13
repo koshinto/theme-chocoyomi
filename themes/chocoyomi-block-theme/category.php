0a6fe4ad042f24b15a1c1344ad2c14621b5e9937
Kosuke Shinto <kou.shinto@icloud.com>
2021-08-13 18:47:14 +0900

    change 検索・カテゴリー一覧のhtml修正

--- themes/chocoyomi-block-theme/category.php
+++ themes/chocoyomi-block-theme/category.php
@@ -2,26 +2,31 @@
 
 <main>
   <div class="container">
-    <p>「<?php single_cat_title() ;?>」の記事</p>
-    <?php if(have_posts()): while(have_posts()): the_post(); ?>
-    <article <?php post_class(); ?>>
+  <?php if(have_posts()): ?>
+    <div class="title">
+      <span class="section-subtitle">Categories</span>
+      <h2 class="section-title"><?php single_cat_title(); ?></h2>
+    </div>
+    <ul class="posts">
+    <?php while(have_posts()): the_post(); ?>
+    <li <?php post_class(); ?>>
       <a href="<?php the_permalink(); ?>">
+        <div class="post-columns">
         <?php if(has_post_thumbnail()): ?>
-        <?php the_post_thumbnail(array(510, 510)); ?>
+          <div class="wp-post-image-wrap"><?php the_post_thumbnail(array(510, 510)); ?></div>
         <?php else: ?>
-        <img class="wp-post-image no-image" src="<?php echo get_template_directory_uri() ?>/assets/no_image.jpg" alt="noimage">
+          <div class="wp-post-image-wrap"><img class="wp-post-image no-image" src="<?php echo get_template_directory_uri() ?>/assets/no_image.jpg" alt="noimage"></div>
         <?php endif; ?>
-        <div class="wp-post-summary">
-          <h1 class="wp-post-title"><?php echo wp_trim_words(get_the_title(), 36, '...') ?></h1>
-          <time class="wp-post-date"><?php the_date(); ?></time>
+          <div class="wp-post-summary">
+            <h1 class="wp-post-title"><?php echo wp_trim_words(get_the_title(), 36, '...') ?></h1>
+            <time class="wp-post-date"><?php the_date(); ?></time>
+          </div>
         </div>
       </a>
-    </article>
+    </li>
     <?php endwhile; ?>
-    <?php else: ?>
-    <p>該当する記事が見つかりませんでした。</p>
+    </ul>
     <?php endif; ?>
-    <?php the_pegination(); ?>
   </div>
 </main>
 
--- themes/chocoyomi-block-theme/search.php
+++ themes/chocoyomi-block-theme/search.php
@@ -2,29 +2,31 @@
 
 <main>
   <div class="container">
-    <?php if(have_posts()): ?>
-    <p>「<?php the_search_query() ;?>」の検索結果</p>
+  <?php if(have_posts()): ?>
+    <div class="title">
+      <span class="section-subtitle">Search</span>
+      <h2 class="section-title">「<?php the_search_query(); ?>」の検索結果</h2>
+    </div>
     <ul class="posts">
     <?php while(have_posts()): the_post(); ?>
     <li <?php post_class(); ?>>
       <a href="<?php the_permalink(); ?>">
+        <div class="post-columns">
         <?php if(has_post_thumbnail()): ?>
-        <?php the_post_thumbnail(array(510, 510)); ?>
+          <div class="wp-post-image-wrap"><?php the_post_thumbnail(array(510, 510)); ?></div>
         <?php else: ?>
-        <img class="wp-post-image no-image" src="<?php echo get_template_directory_uri() ?>/assets/no_image.jpg" alt="noimage">
+          <div class="wp-post-image-wrap"><img class="wp-post-image no-image" src="<?php echo get_template_directory_uri() ?>/assets/no_image.jpg" alt="noimage"></div>
         <?php endif; ?>
-        <div class="wp-post-summary">
-          <h1 class="wp-post-title"><?php echo wp_trim_words(get_the_title(), 36, '...') ?></h1>
-          <time class="wp-post-date"><?php the_date(); ?></time>
+          <div class="wp-post-summary">
+            <h1 class="wp-post-title"><?php echo wp_trim_words(get_the_title(), 36, '...') ?></h1>
+            <time class="wp-post-date"><?php the_date(); ?></time>
+          </div>
         </div>
       </a>
     </li>
     <?php endwhile; ?>
     </ul>
-    <?php else: ?>
-    <p>該当する記事が見つかりませんでした。</p>
     <?php endif; ?>
-    <?php the_pegination(); ?>
   </div>
 </main>
 
