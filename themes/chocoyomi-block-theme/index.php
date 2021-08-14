dc910dd297580c29ed473e25302407fa906efd2f
Kosuke Shinto <kou.shinto@icloud.com>
2021-08-14 15:14:07 +0900

    add 編集部のお知らせ

--- themes/chocoyomi-block-theme/index.php
+++ themes/chocoyomi-block-theme/index.php
@@ -82,23 +82,29 @@
       $query = new WP_Query($args);
       if($query->have_posts()):
     ?>
-    <span class="section-subtitle">Recommended by Editorial</span>
-    <h3 class="section-title">編集部のおすすめ</h3>
+    <div class="title">
+      <span class="section-subtitle">Notice by Editorial</span>
+      <h3 class="section-title">編集部のお知らせ</h3>
+    </div>
+    <ul class="posts notice-by-editorial">
     <?php while($query->have_posts()): $query->the_post(); ?>
-    <article <?php post_class(); ?>>
-      <a href="<?php the_permalink(); ?>">
-        <?php if(has_post_thumbnail()): ?>
-        <?php the_post_thumbnail(array(510, 510)); ?>
-        <?php else: ?>
-          <img class="wp-post-image no-image" src="<?php echo get_template_directory_uri() ?>/assets/no_image.jpg" alt="noimage">
-        <?php endif; ?>
-          <div class="wp-post-summary">
-            <h1 class="wp-post-title"><?php echo wp_trim_words(get_the_title(), 36, '...') ?></h1>
-            <time class="wp-post-date"><?php the_date(); ?></time>
+      <li <?php post_class(); ?>>
+        <a href="<?php the_permalink(); ?>">
+          <div class="post-columns">
+          <?php if(has_post_thumbnail()): ?>
+            <div class="wp-post-image-wrap"><?php the_post_thumbnail(array(510, 510)); ?></div>
+          <?php else: ?>
+            <div class="wp-post-image-wrap"><img class="wp-post-image no-image" src="<?php echo get_template_directory_uri() ?>/assets/no_image.jpg" alt="noimage"></div>
+          <?php endif; ?>
+            <div class="wp-post-summary">
+              <h1 class="wp-post-title"><?php echo wp_trim_words(get_the_title(), 36, '...') ?></h1>
+              <time class="wp-post-date"><?php the_date(); ?></time>
+            </div>
           </div>
-      </a>
-    </article>
+        </a>
+      </li>
     <?php endwhile; endif; ?>
+    </div>
   </div>  <!-- container閉じタグ -->
 </main>
 
