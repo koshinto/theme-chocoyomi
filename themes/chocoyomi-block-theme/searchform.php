7044e7ca21d92efacef579f8329e0b32d7cebfa8
Kosuke Shinto <kou.shinto@icloud.com>
2021-08-24 18:01:49 +0900

    fix ID: search-formが複数存在

--- themes/chocoyomi-block-theme/searchform.php
+++ themes/chocoyomi-block-theme/searchform.php
@@ -1,4 +1,4 @@
-<form method="get" id="search-form" action="<?php echo esc_url(home_url('/')); ?>">
+<form method="get" class="search-form" action="<?php echo esc_url(home_url('/')); ?>">
   <input type="search" placeholder="キーワード検索" name="s" value="" />
   <button type="submit"><i class="fas fa-search"></i>検索</button>
 </form>
